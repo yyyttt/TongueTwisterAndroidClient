76e6de694d25192557459ef837ade9e0ba94a9b9
LiWenting <18292003113@126.com>
2016-08-02 21:50:16 +0800

    录音权限问题，已解决； 不录音也可点击右下角播放录音按钮，已解决； 申请多余的权限，如电话权限，已去除

--- TongueTwister/src/team/abc/tonguetwister/widget/ShowMaterialDialog.java
+++ TongueTwister/src/team/abc/tonguetwister/widget/ShowMaterialDialog.java
@@ -5,23 +5,40 @@ import com.flyco.animation.SlideExit.SlideBottomExit;
 import com.flyco.dialog.listener.OnBtnClickL;
 import com.flyco.dialog.widget.MaterialDialog;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.text.Html;
 import team.abc.tonguetwister.R;
+import team.abc.tonguetwister.activity.PassThroughActivity;
+import team.abc.tonguetwister.application.MyApplication;
 import team.abc.tonguetwister.constant.Constant;
 import team.abc.tonguetwister.service.DownloadService;
 
 public class ShowMaterialDialog {
-	public static void showMaterialDialog(String msg,Context mContext) {
+	//一个按钮
+	public static void showMaterialDialog(String msg,final Activity mactivity,Context mContext) {
 		final MaterialDialog dialog = new MaterialDialog(
 				mContext);
 
-		dialog.btnNum(1).content(msg).btnText("确定")
+		dialog.btnNum(1).content(msg).btnText("知道了")
 				.showAnim(new BounceTopEnter())
 				.dismissAnim(new SlideBottomExit()).show();
+		if (msg.equals(Constant.RECORD_DENIED)) {
+			dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
 
+				@Override
+				public void onDismiss(DialogInterface dialog) {
+					dialog.cancel();
+					mactivity.startActivity(new Intent(mactivity, PassThroughActivity.class));
+					mactivity.overridePendingTransition(R.anim.push_right_in,
+							R.anim.push_right_out);
+					mactivity.finish();
+				}
+			});
+		}
+		else{
 		dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
 
 			@Override
@@ -29,33 +46,34 @@ public class ShowMaterialDialog {
 				dialog.cancel();
 
 			}
-		});
+		});}
 		 dialog.setCanceledOnTouchOutside(false);
 	}
-	
+	//两个按钮
 	public static void showMaterialDialog2(final Context mContext,String msg,final String downloadUrl) {
 		final MaterialDialog dialog = new MaterialDialog(
 				mContext);
 		
-		dialog.setTitle("发现新版本");
-		dialog.btnNum(2).content(Html.fromHtml(msg).toString()).btnText("以后再说","立即下载")
-				.showAnim(new BounceTopEnter())
-				.dismissAnim(new SlideBottomExit()).show();
-		dialog.setCanceledOnTouchOutside(false);
-	dialog.setOnBtnClickL(new OnBtnClickL() {
-		
-		@Override
-		public void onBtnClick() {
-			dialog.dismiss();
-		}
-	},new OnBtnClickL() {
-		
-		@Override
-		public void onBtnClick() {
-			 goToDownload(mContext, downloadUrl);
-			dialog.dismiss();
-		}
-	});
+//		dialog.setTitle("发现新版本");
+			dialog.btnNum(2).content(Html.fromHtml(msg).toString()).btnText("以后再说","立即下载")
+			.showAnim(new BounceTopEnter())
+			.dismissAnim(new SlideBottomExit()).show();
+	        dialog.setCanceledOnTouchOutside(false);
+            dialog.setOnBtnClickL(new OnBtnClickL() {
+	
+	        @Override
+	        public void onBtnClick() {
+		    dialog.dismiss();
+	        }
+            },new OnBtnClickL() {
+	
+	       @Override
+	       public void onBtnClick() {
+		   goToDownload(mContext, downloadUrl);
+		   dialog.dismiss();
+	       }
+           });
+	
 		 
 	}
     private static void goToDownload(Context context, String downloadUrl) {
