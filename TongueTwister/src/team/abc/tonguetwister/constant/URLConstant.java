04534bf7425f1fc986116631910ad948b00ed2c5
Albert <562494369@qq.com>
2016-08-06 16:51:26 +0800

    张申聪： 解决问题单20160726 服务器： 1、（解决）在线升级 和 随时分享无服务器监控。
    构建服务端框架：
    1、同一ip不能在10秒内重复访问。
    2、日志文件记录。

--- TongueTwister/src/team/abc/tonguetwister/constant/URLConstant.java
+++ TongueTwister/src/team/abc/tonguetwister/constant/URLConstant.java
@@ -5,7 +5,9 @@ public class URLConstant {
 	private static final String BASE = "http://182.61.51.97:8080";
 	public static final String LOGIN_URL = BASE + "/NSPClient";
 	public static final String LOGOUT_URL = BASE + "/NSPClient/logout.html";
-	//app下载网址
-	public static final String SHARE_URL = BASE+"/Download/TongueTwister.apk";
-	public static final String UPDATE_URL = BASE+"/Download/update.json";
+	
+	//app分享下载网址（有监控和鉴权）
+	public static final String SHARE_URL = BASE+"/TongueTwister/APKDownload";
+	//app更新网址（有监控）
+	public static final String UPDATE_URL = BASE+"/TongueTwister/APKUpdate";
 }
